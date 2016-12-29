680754f2133c8ba5f952425f0c13a66b396395a5
bigwheel <k.bigwheel+eng@gmail.com>
2016-12-30 03:00:37 +0900

    zsh自体のインストールコマンドを追加

--- setup.sh
+++ setup.sh
@@ -5,5 +5,7 @@ set -eux
 # http://qiita.com/yudoufu/items/48cb6fb71e5b498b2532
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
+sudo apt-get install -y zsh
+
 git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 ln -s $script_dir/my-zshrc.zsh $HOME/.oh-my-zsh/custom/my-zshrc.zsh
