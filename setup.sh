0a3ae640aafc074186a1c057c24301ca2e257bf8
bigwheel <k.bigwheel+eng@gmail.com>
2016-12-31 19:40:22 +0900

    sudo権限を要求するzsh自体のインストールを削除

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ set -eux
 # http://qiita.com/yudoufu/items/48cb6fb71e5b498b2532
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
-sudo apt-get install -y zsh
-
+# oh-my-zsh代替を使用することでvimやtmuxと同じく起動時に動的に取ってくることができそう
 git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 ln -s $script_dir/my-zshrc.zsh $HOME/.oh-my-zsh/custom/my-zshrc.zsh
